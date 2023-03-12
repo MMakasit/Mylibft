4bd1e162b079853b4c56f1fce6e194d963e03715
MMaliwan <suchakree88@gmail.com>
2023-03-12 21:47:16 +0700

    fistcommit

---
+++ test.c
@@ -0,0 +1,15 @@
+#include "libft.h"
+
+int main(void)
+{
+    int n = ft_isalpha('0');
+    if(n == 1)
+    {
+        printf("as alphabet");
+    }
+    else
+    {
+        printf("not an alphabet");
+    }
+
+}
